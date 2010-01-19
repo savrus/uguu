c5abad304da031c67dae7846f39c16cfe38906f9
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-01-19 23:11:06 +0300

    Switch to new DDL
    New ddl is described on wiki and in dbinit.py. Major diference from previous
    one is dropping small tables from database and leaving them to python
    sctripts.

--- webuguu/vfs/views.py
+++ webuguu/vfs/views.py
@@ -34,7 +34,7 @@ def net(request):
     except:
         return HttpResponse("Unable to connect to the database.")
     cursor = db.cursor()
-    cursor.execute("SELECT network_id, name FROM network")
+    cursor.execute("SELECT network_name FROM networks")
     return render_to_response('vfs/net.html', \
         {'networks': cursor.fetchall()})
 
@@ -48,16 +48,11 @@ def network(request, network):
         return HttpResponse("Unable to connect to the database.")
     cursor = db.cursor()
     cursor.execute("""
-        SELECT share.share_id, host.name, size, proto.name
-        FROM host
-        LEFT JOIN share ON (host.host_id = share.host_id)
-        LEFT JOIN sharetype ON (share.sharetype_id = sharetype.sharetype_id)
-        LEFT JOIN proto ON (sharetype.proto_id = proto.proto_id)
-        LEFT JOIN file ON (share.share_id = file.share_id
-                           AND file.parent_within_share_id = 0)
-        WHERE host.network_id =
-            (SELECT network_id FROM network WHERE name = %(n)s)
-        ORDER BY host.host_id
+        SELECT shares.share_id, hosts.name, shares.size, shares.protocol
+        FROM hosts
+        LEFT JOIN shares ON (hosts.host_id = shares.host_id)
+        WHERE hosts.network_name = %(n)s
+        ORDER BY hosts.host_id
         """, {'n':network})
     return render_to_response('vfs/network.html', \
         {'shares': cursor.fetchall()})
@@ -74,56 +69,41 @@ def share(request, proto, hostname, path=""):
     #TODO split hostname into hostname and port
     cursor.execute("""
         SELECT share_id
-        FROM share
-        WHERE sharetype_id =
-                (SELECT sharetype_id
-                FROM sharetype
-                LEFT JOIN proto ON (sharetype.proto_id = proto.proto_id)
-                WHERE proto.name = %(p)s)
+        FROM shares
+        WHERE protocol = %(p)s
             AND host_id =
-            (SELECT host_id FROM host WHERE name = %(h)s)
+            (SELECT host_id FROM hosts WHERE name = %(h)s)
         """, {'p': proto, 'h': hostname})
     try:
         share_id, = cursor.fetchone()
     except:
         return HttpResponse("Unknown share");
     cursor.execute("""
-        SELECT path_within_share_id, parent_id
-        FROM path
+        SELECT sharepath_id, parent_id
+        FROM paths
         WHERE share_id = %(s)s AND path = %(p)s
         """, {'s': share_id, 'p': path})
     try:
         path_id, parent_id = cursor.fetchone()
     except:
         return HttpResponse("No such file or directory '" + path + "'")
-    #cursor.execute("""
-    #    SELECT path
-    #    FROM path
-    #    WHERE share_id = %(s)s AND path_within_share_id = %(p)s
-    #    """, {'s':share_id, 'p':parent_id})
-    #try:
-    #    path, = cursor.fetchone()
-    #    out = "Up: " + path + "<br><br>"
-    #except:
-    #    out = ""
     if parent_id == 0:
         cursor.execute("""
-            SELECT network.name
-            FROM share
-            LEFT JOIN host ON (share.host_id = host.host_id)
-            LEFT JOIN network ON (host.network_id = network.network_id)
+            SELECT network_name
+            FROM shares
+            LEFT JOIN hosts ON (shares.host_id = hosts.host_id)
             WHERE share_id = %(s)s
         """, {'s': share_id})
         network, = cursor.fetchone()
     else:
         network = ""
     cursor.execute("""
-        SELECT path_within_share_id, size, name
-        FROM file
-        LEFT JOIN filename ON (file.filename_id = filename.filename_id)
+        SELECT sharedir_id, size, name
+        FROM files
+        LEFT JOIN filenames ON (files.filename_id = filenames.filename_id)
         WHERE share_id = %(s)s
-            AND parent_within_share_id = %(p)s
-        ORDER BY file_within_path_id;
+            AND sharepath_id = %(p)s
+        ORDER BY pathfile_id;
         """, {'s': share_id, 'p': path_id})
     return render_to_response('vfs/share.html', \
         {'files': cursor.fetchall(), 'network': network})
