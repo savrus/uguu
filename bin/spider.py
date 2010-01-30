bfbbfb3c60d9530daa79f3de728bfa2de3811eb6
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-01-31 01:33:15 +0300

    [spider.py][search] Python regex do not work with unicode well...

--- bin/spider.py
+++ bin/spider.py
@@ -37,7 +37,7 @@ def suffix(filename):
 
 
 def tsprepare(string):
-    relax = re.sub(r'\W', ' ', string)
+    relax = re.sub(r'\.\,\[\]\(\)\-', ' ', string)
     relax = re.sub(r'([Ss])(\d+)([Ee])(\d+)',
                    '\\1\\2\\3\\4 \\2 \\4', relax)
     return relax
