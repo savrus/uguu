82dd8c0013998fdfaddff35caee1bf6a56b817f6
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-02-03 02:02:07 +0300

    [dbinit.py][vfs] Sort shares in each network. Split shares list to pages
    Add total counter to each network, add netshare_id to each share, add triggers
    to keep networks.total and shares.netshare_id in conjunction with
    shares.hostname.
    Add gobar to shares list in vfs.

--- bin/dbinit.py
+++ bin/dbinit.py
@@ -22,7 +22,8 @@ def ddl(db):
     cursor = db.cursor()
     cursor.execute("""
         CREATE TABLE networks (
-            network varchar(32) PRIMARY KEY
+            network varchar(32) PRIMARY KEY,
+            total integer DEFAULT 0
         );
         CREATE TABLE scantypes (
             scantype_id SERIAL PRIMARY KEY,
@@ -30,6 +31,7 @@ def ddl(db):
         );
         CREATE TABLE shares (
             share_id SERIAL PRIMARY KEY,
+            netshare_id integer,
             scantype_id integer REFERENCES scantypes ON DELETE RESTRICT,
             network varchar(32) REFERENCES networks ON DELETE CASCADE,
             protocol varchar(8),
@@ -86,6 +88,78 @@ def ddl_prog(db):
         CREATE TRIGGER share_stage_change_trigger
             BEFORE UPDATE ON shares FOR EACH ROW
             EXECUTE PROCEDURE share_state_change();
+
+        CREATE OR REPLACE FUNCTION share_handle_netshare_id()
+            RETURNS trigger AS
+            $$BEGIN
+                IF (TG_OP = 'INSERT') THEN
+                    UPDATE shares SET netshare_id = netshare_id + 1
+                        WHERE shares.network = NEW.network
+                            AND (shares.hostname, shares.protocol, shares.port)
+                                > (NEW.hostname, NEW.protocol, NEW.port);
+                    NEW.netshare_id = (SELECT max(netshare_id) + 1 FROM
+                        (SELECT netshare_id FROM shares
+                            WHERE shares.network = NEW.network
+                            AND (shares.hostname, shares.protocol, shares.port)
+                                <= (NEW.hostname, NEW.protocol, NEW.port)
+                         UNION SELECT -1) AS m);
+                    RETURN NEW;
+                ELSIF (TG_OP = 'DELETE') THEN
+                    UPDATE shares SET netshare_id = netshare_id - 1
+                        WHERE shares.network = OLD.network
+                            AND shares.netshare_id > OLD.netshare_id;
+                    RETURN OLD;
+                ELSIF (TG_OP = 'UPDATE') THEN
+                    IF ( NEW.network != OLD.network
+                            OR NEW.hostname != OLD.hostname
+                            OR NEW.protocol != OLD.protocol
+                            OR NEW.port != OLD.port) THEN
+                        UPDATE shares SET netshare_id = netshare_id - 1
+                            WHERE shares.network = OLD.network
+                                AND shares.netshare_id > OLD.netshare_id;
+                        UPDATE shares SET netshare_id = netshare_id + 1
+                            WHERE shares.network = NEW.network
+                            AND (shares.hostname, shares.protocol, shares.port)
+                                > (NEW.hostname, NEW.protocol, NEW.port);
+                        NEW.netshare_id = (SELECT max(netshare_id) + 1 FROM
+                            (SELECT netshare_id FROM shares
+                                WHERE shares.network = NEW.network
+                                AND (shares.hostname, shares.protocol, shares.port)
+                                <= (NEW.hostname, NEW.protocol, NEW.port)
+                            UNION SELECT -1) AS m);
+                    END IF;
+                    RETURN NEW;
+                END IF;
+            END;$$
+            LANGUAGE 'plpgsql';
+        CREATE OR REPLACE FUNCTION network_handle_netshare_id()
+            RETURNS trigger AS
+            $$BEGIN
+                IF (TG_OP = 'INSERT') THEN
+                    UPDATE networks SET total = total + 1
+                        WHERE networks.network = NEW.network;
+                    RETURN NEW;
+                ELSIF (TG_OP = 'DELETE') THEN
+                    UPDATE networks SET total = total - 1
+                        WHERE networks.network = NEW.network;
+                    RETURN OLD;
+                ELSIF (TG_OP = 'UPDATE') THEN
+                    IF ( NEW.network != OLD.network ) THEN
+                        UPDATE networks SET total = total - 1
+                            WHERE networks.network = NEW.network;
+                        UPDATE networks SET total = total + 1
+                            WHERE networks.network = NEW.network;
+                    END IF;
+                    RETURN NEW;
+                END IF;
+            END;$$
+            LANGUAGE 'plpgsql';
+        CREATE TRIGGER share_handle_netshare_id_trigger
+            BEFORE INSERT OR DELETE OR UPDATE ON shares FOR EACH ROW
+            EXECUTE PROCEDURE share_handle_netshare_id();
+        CREATE TRIGGER network_handle_netshare_id_trigger
+            AFTER INSERT OR DELETE OR UPDATE ON shares FOR EACH ROW
+            EXECUTE PROCEDURE network_handle_netshare_id();
 	""")
 
 
