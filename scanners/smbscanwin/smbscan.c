7567dcc7d6f8369255373dc3dae6d5965738385f
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-02-03 00:16:25 +0300

    Add my copyright to scripts common.py

---
+++ scanners/smbscanwin/smbscan.c
@@ -0,0 +1,66 @@
+/* smbscan - smb scanner
+ *
+ * Copyright 2009, 2010, savrus
+ * Copyright 2010, Radist <radist.nt@gmail.com>
+ * Read the COPYING file in the root of the source tree.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <crtdbg.h>
+
+#include "dt.h"
+#include "smbwk.h"
+
+static void usage(wchar_t *binname, int err)
+{
+    fprintf(stderr, "Usage: %S [-f] host\n", binname);
+    fprintf(stderr, "\t-f\tprint full paths (debug output)\n");
+    exit(err);
+}
+
+int wmain(int argc, wchar_t **argv)
+{
+    struct dt_dentry d = {DT_DIR, "", 0, NULL, NULL, NULL, 0};
+    struct smbwk_dir curdir;
+    int full = 0;
+    wchar_t *host, *user = NULL, *pass = NULL;
+
+#if defined(_DEBUG)
+	_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_DEBUG );
+	_CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_DEBUG );
+	_CrtSetReportMode( _CRT_ASSERT, _CRTDBG_MODE_DEBUG );
+	_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG));
+#endif // _DEBUG
+
+    if (argc < 2)
+        usage(argv[0], EXIT_FAILURE);
+    
+    host = argv[1];
+
+    if (wcscmp(argv[1], L"-f") == 0) {
+        if (argc < 3)
+            usage(argv[0], EXIT_FAILURE);
+        full = 1;
+        host = argv[2];
+    }
+
+	if (!user)
+		user = L"Guest";
+	if (!pass)
+		pass = L"";
+
+    if (smbwk_open(&curdir, host, user, pass, ENUM_SKIP_DOLLAR) < 0)
+        return EXIT_FAILURE;
+    
+    if (full)
+        dt_full(&smbwk_walker, &d, &curdir);
+    else
+        dt_reverse(&smbwk_walker, &d, &curdir);
+
+    smbwk_close(&curdir);
+
+    return EXIT_SUCCESS;
+}
+
---
+++ webuguu/search/views.py
@@ -0,0 +1,196 @@
+# webuguu.search.views - search view for django framework
+#
+# Copyright 2010, savrus
+# Read the COPYING file in the root of the source tree.
+#
+
+from django.utils.http import urlencode
+from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
+import string
+import re
+from webuguu.common import connectdb, generate_go_bar, vfs_items_per_page, search_items_per_page
+
+usertypes = (
+    {'value':'',                        'text':'All'},
+    {'value':'type:video min:300Mb',    'text':'Films'},
+    {'value':'type:video max:400Mb',    'text':'Clips'},
+    {'value':'type:audio',              'text':'Audio'},
+    {'value':'type:archive',            'text':'Archives'},
+    {'value':'type:dir',          'text':'Directories'}
+)
+
+conditions = {
+    'video':    " AND filenames.type = %(type)s",
+    'audio':    " AND filenames.type = %(type)s",
+    'archive':  " AND filenames.type = %(type)s",
+    'dir':      " AND files.sharedir_id > 0"
+}
+
+def size2byte(size):
+    sizenotatios = {'b':1, 'k':1024, 'm':1024*1024, 'g':1024*1024*1024,
+                    'kb':1024, 'mb':1024*1024, 'gb':1024*1024*1024}
+    m =  re.match(r'(\d+)(\w+)', size).groups()
+    s = int(m[0])
+    if m[1]:
+        s *= sizenotatios.get(string.lower(m[1]), 1)
+    return s
+
+class QueryParser:
+    def __init__(self, query):
+        self.options = dict()
+        self.options['query'] = ""
+        for w in re.findall(r'(?u)(\w+)(:(?:\w|\d|\.)*)?', query):
+            if w[1] == "":
+                if self.options['query'] != "":
+                    self.options['query'] += " & "
+                self.options['query'] += w[0] + ":*"
+            elif w[0] in ['type', 'max', 'min', 'full', 'host', 'proto']:
+                self.options[w[0]] = w[1][1:]
+        self.sqlquery = "WHERE"
+        fullpath = self.options.get("full","")
+        if fullpath != "":
+            self.sqlquery += " paths.tspath ||"
+        self.sqlquery += " filenames.tsname @@ to_tsquery('uguu',%(query)s)"
+        type = self.options.get("type", "")
+        if type != "" and conditions.get(type, "") != "":
+            self.sqlquery += conditions[type]
+        max = self.options.get("max")
+        if max != None:
+            self.sqlquery += " AND files.size < %(max)s"
+            self.options['max'] = size2byte(max)
+        min = self.options.get("min")
+        if min != None:
+            self.sqlquery += " AND files.size > %(min)s"
+            self.options['min'] = size2byte(min)
+        host = self.options.get("host", "")
+        if host != "":
+            self.sqlquery += " AND shares.hostname = %(host)s"
+        proto = self.options.get("proto", "")
+        if proto != "":
+            self.sqlquery += " AND shares.protocol = %(proto)s"
+    def setoption(self, opt, val):
+        self.options[opt] = val
+    def sqlwhere(self):
+        return self.sqlquery
+    def sqlcount(self):
+        str = ""
+        if self.options.get("full", "") != "":
+            str += """
+                JOIN paths ON (files.share_id = paths.share_id
+                    AND files.sharepath_id = paths.sharepath_id)
+                """
+        if self.options.get("host", "") != "" or \
+           self.options.get("host", "") != "":
+            str += """
+                JOIN shares ON (files.share_id = shares.share_id)
+                """
+        return str + self.sqlquery
+    def sqlsubs(self):
+        return self.options
+
+
+def do_search(request, index, searchform):
+    try:
+        query = request.GET['q']
+    except:
+        return render_to_response(index,
+            {'form': searchform, 'types': usertypes})
+    try:
+        db = connectdb()
+    except:
+        return render_to_response('search/error.html',
+            {'error':"Unable to connect to the database."})
+    cursor = db.cursor()
+    type = request.GET.get('t', "")
+    types = []
+    for t in usertypes:
+        nt = dict(t)
+        nt['selected'] = 'selected="selected"' if nt['value'] == type else ""
+        types.append(nt)
+    parsedq = QueryParser(query + " " + type)
+    cursor.execute("""
+        SELECT count(*) as count
+        FROM filenames
+        JOIN files on (filenames.filename_id = files.filename_id)
+        """ + parsedq.sqlcount(), parsedq.sqlsubs())
+    items = int(cursor.fetchone()['count'])
+    page_offset = int(request.GET.get('o', 0))
+    offset = page_offset * search_items_per_page
+    gobar = generate_go_bar(items, page_offset)
+    parsedq.setoption("offset", offset)
+    parsedq.setoption("limit", search_items_per_page)
+    cursor.execute("""
+        SELECT protocol, hostname,
+            paths.path AS path, files.sharedir_id AS dirid,
+            filenames.name AS filename, files.size AS size, port,
+            shares.share_id, paths.sharepath_id as path_id,
+            files.pathfile_id as fileid, shares.state
+        FROM filenames
+        JOIN files ON (filenames.filename_id = files.filename_id)
+        JOIN paths ON (files.share_id = paths.share_id
+            AND files.sharepath_id = paths.sharepath_id)
+        JOIN shares ON (files.share_id = shares.share_id)
+        """ + parsedq.sqlwhere() + """
+        ORDER BY shares.state DESC, files.share_id, files.sharepath_id,
+            files.pathfile_id
+        OFFSET %(offset)s LIMIT %(limit)s
+        """, parsedq.sqlsubs())
+    if cursor.rowcount == 0:
+        return render_to_response('search/error.html',
+            {'form': searchform, 'types': types, 'query': query,
+             'error':"Sorry, nothing found."})
+    else:
+        res = cursor.fetchall()
+        result = []
+        for row in res:
+            newrow = dict()
+            urlpath = "/" + row['path'] if row['path'] != "" else ""
+            urlhost = row['hostname']
+            urlhost += ":" + str(row['port']) if row['port'] != 0 else ""
+            ##change 'smb' to 'file' here
+            #if row['protocol'] == "smb":
+            #    urlproto = "file"
+            #else:
+            #    urlproto = row['protocol']
+            urlproto = row['protocol']
+            viewargs = [row['protocol'], row['hostname'], row['port']]
+            if row['path'] != "":
+                viewargs.append(row['path'])
+            vfs = reverse('webuguu.vfs.views.share', args=viewargs)
+            vfs_offset = int(row['fileid']) / vfs_items_per_page
+            newrow['pathlink'] = vfs + "?" + urlencode(dict(
+                [('s', row['share_id']), ('p', row['path_id'])] +
+                ([('o', vfs_offset)] if vfs_offset > 0 else []) ))
+            newrow['filename'] = row['filename']
+            if row['dirid'] > 0:
+                newrow['type'] = "<dir>"
+                newrow['filelink'] = vfs + newrow['filename'] + "/?" + \
+                    urlencode({'s': row['share_id'], 'p': row['dirid']})
+            else:
+                newrow['type'] = ""
+                newrow['filelink'] = urlproto + "://" +\
+                    urlhost + urlpath + "/" + newrow['filename']
+            newrow['path'] = row['protocol'] + "://" + urlhost + urlpath
+            newrow['size'] = row['size']
+            newrow['state'] = "online" if row['state'] else "offline"
+            result.append(newrow)
+            del row
+        del res
+        fastselflink = "./?" + urlencode(dict([('q', query), ('t', type)]))
+        return render_to_response('search/results.html',
+            {'form': searchform,
+             'query': query,
+             'types': types,
+             'results': result,
+             'offset': offset,
+             'fastself': fastselflink,
+             'gobar': gobar
+             })
+
+def search(request):
+    return do_search(request, "search/index.html", "search/searchform.html")
+
+def light(request):
+    return do_search(request, "search/lightindex.html", "search/lightform.html")
+
