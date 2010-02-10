ee53c4ae9fd05ce5ee4c40d577908549dbb408b4
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-02-10 04:41:55 +0300

    [dbinit.py][spider.py] Some madskillz optimizations.
    Melchior scanning time reduced by factor 3.5.
    It appears that hudge bandwidth between python and postgres is not handled
    well by psycopg2 (or at least we don't know how to use it properly).

---
+++ bin/pinger.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+#
+# pinger.py - online checker
+#
+# Copyright (c) 2010, Radist <radist.nt@gmail.com>
+# Read the COPYING file in the root of the source tree.
+#
+
+import psycopg2
+import sys
+import socket
+import subprocess
+import string
+import re
+from network import dns_cache, scan_all_hosts
+from common import connectdb, default_ports
+
+def get_names_list(ips, nscache):
+    res = set()
+    for ip in ips:
+        res|=nscache(None, ip)
+    return res
+
+def check_online_shares(hostlist, port):
+    nscache = dns_cache()
+    iplist = frozenset([nscache(host) for host in hostlist])
+    hostlist = frozenset(hostlist)
+    online = set(ip for (ip, port) in scan_all_hosts([(ip, port) for ip in iplist]))
+    return get_names_list(online, nscache) & hostlist, \
+           get_names_list(iplist - online, nscache) & hostlist
+
+def update_shares_state(db, selwhere, port):
+    cursor = db.cursor()
+    cursor.execute("SELECT share_id, hostname FROM shares WHERE %s" % selwhere)
+    itemdict = dict([(row['hostname'], row['share_id']) for row in cursor.fetchall()])
+    if len(itemdict) == 0:
+        return
+    online, offline = check_online_shares(itemdict.keys(), port)
+    if len(online):
+        cursor.execute("UPDATE shares SET state='online' WHERE share_id IN %s", \
+        	(tuple(itemdict[host] for host in online),))
+    if len(offline):
+        cursor.execute("UPDATE shares SET state='offline' WHERE share_id IN %s", \
+        	(tuple(itemdict[host] for host in offline),))
+
+def get_shares_ports(db):
+    cursor = db.cursor()
+    cursor.execute("SELECT DISTINCT(port) FROM shares WHERE port>0")
+    return [p[0] for p in cursor.fetchall()]
+
+if __name__ == "__main__":
+    try:
+        db = connectdb()
+    except:
+        print "I am unable to connect to the database, exiting."
+        sys.exit()
+
+    for proto in default_ports.iteritems():
+        update_shares_state(db, "protocol='%s' AND port=0" % proto[0], proto[1])
+
+    for port in get_shares_ports(db):
+        update_shares_state(db, "port=%d" % port, port)
+
+    db.commit()
---
+++ scanners/ftpscan/ftpscan.cpp
@@ -0,0 +1,246 @@
+/* ftpscan - ftp scanner
+ *
+ * Copyright (c) 2010, Radist <radist.nt@gmail.com>
+ * All rights reserved.
+ *
+ * Read the COPYING file in the root of the source tree.
+ */
+
+#ifdef _WIN32
+
+#include <Windows.h>
+
+#define usleep Sleep
+#ifdef _MSC_VER
+#define strdup _strdup
+#endif
+
+#else
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#endif//_WIN32
+
+#include <stdio.h>
+#include <string.h>
+#include <string>
+
+#include "dt.h"
+#include "logpp.h"
+#include "FtpSockLib.h"
+
+#define DEFAULT_ANSI_CODEPAGE "latin1"
+
+//////////////////////////////////////////////////////////////////////////
+// dt_walker routines
+
+class CFtpControlEx
+: public CFtpControl
+{
+public:
+	CFtpControlEx() : curpath("/"), do_list(true), errors(10) {}
+	~CFtpControlEx() {
+		if (!do_list) FindClose(findinfo);
+	}
+	std::string curpath;
+	bool do_list;
+	FtpFindInfo findinfo;
+	int errors;
+};
+
+#define SAFE_FTP_CALL(...) \
+	while(1) { \
+		try { \
+			__VA_ARGS__ \
+		} catch(const CFtpControl::NetworkError) { \
+			if( ftp->errors--<0 ) \
+				throw CFtpControl::NetworkError(); \
+			usleep(1000); \
+			TryReconnect(ftp); \
+		} \
+	}
+
+static void TryReconnect(CFtpControlEx *ftp)
+{
+	ftp->Quit();
+	if( !ftp->tryconn() ) {
+		LOG_ERR("Cannot connect %d.%d.%d.%d:%d\n", 
+			 ftp->ServerIP        & 0xFF,
+			(ftp->ServerIP >>  8) & 0xFF,
+			(ftp->ServerIP >> 16) & 0xFF,
+			 ftp->ServerIP >> 24,
+			 ftp->ServerPORT);
+		throw CFtpControl::NetworkError();
+	}
+	if( !ftp->Logon() ) {
+		LOG_ERR("Cannot logon: %s\n", ftp->GetLastResponse());
+		throw CFtpControl::NetworkError();
+	}
+	ftp->ChDir(ftp->curpath.c_str());
+}
+
+static struct dt_dentry *fill_dentry(FtpFindInfo &fi)
+{
+	struct dt_dentry *result = dt_alloc();
+	result->type = fi.Data.flagtrycwd?DT_DIR:DT_FILE;
+	result->name = strdup(fi.Data.name);
+	if( DT_FILE == result->type )
+		result->size = fi.Data.size;
+	return result;
+}
+
+
+static struct dt_dentry * ftp_readdir_fn(CFtpControlEx *ftp)
+{
+	do {
+		if(ftp->do_list) {
+			SAFE_FTP_CALL(
+				if( ftp->FindFirstFile(ftp->curpath.c_str(), ftp->findinfo) ) {
+					ftp->do_list = false;
+					break;
+				} else return NULL;
+			)
+		} else {
+			if( !ftp->FindNextFile(ftp->findinfo) ) {
+				ftp->FindClose(ftp->findinfo);
+				ftp->do_list = true;
+				return NULL;
+			}
+		}
+	} while( !strcmp(".", ftp->findinfo.Data.name) || !strcmp("..", ftp->findinfo.Data.name) );
+	return fill_dentry(ftp->findinfo);
+}
+
+static int ftp_go_somewhere(dt_go type, char *name, CFtpControlEx *ftp)
+{
+	ftp->do_list = true;
+	std::string olddir = ftp->curpath;
+	if (DT_GO_CHILD != type)
+		ftp->curpath.resize(ftp->curpath.rfind("/", ftp->curpath.size()-2)+1);
+	if (DT_GO_PARENT != type) {
+		ftp->curpath += name;
+		ftp->curpath += "/";
+	}
+	SAFE_FTP_CALL(
+		if( ftp->ChDir(ftp->curpath.c_str()) ) return 1;
+		else {
+			LOG_ERR("CWD failed for \"%s\", response is\n\t%s", ftp->curpath.c_str(), ftp->GetLastResponse());
+			ftp->curpath = olddir;
+			return -1;
+		}
+	)
+}
+
+static struct dt_walker walker = {
+    (dt_readdir_fn) &ftp_readdir_fn,
+    (dt_go_fn) &ftp_go_somewhere,
+};
+
+//////////////////////////////////////////////////////////////////////////
+// main
+//////////////////////////////////////////////////////////////////////////
+
+#define __TOSTRING(x) #x
+#define _TOSTRING(x) __TOSTRING(x)
+#ifdef _WIN32
+#define _DIR_SLASH '\\'
+#else
+#define _DIR_SLASH '/'
+#endif
+
+static void usage(char *binname, int err)
+{
+    char *bin = strrchr(binname, _DIR_SLASH);
+    if (bin) binname = bin + 1;
+    fprintf(stderr, "Usage: %s [-l] [-f] [(-c|-C) cp] [-P##] [-t###] [-u username] [-p password] [-h] host_ip\n", binname);
+    fprintf(stderr, "\t-l\tlookup mode (detect if there is anything available)\n");
+    fprintf(stderr, "\t-f\tprint full paths (debug output)\n");
+    fprintf(stderr, "\t-c cp\tset codepage for non-utf8 servers (default is " DEFAULT_ANSI_CODEPAGE ")\n");
+    fprintf(stderr, "\t-C cp\tforce server codepage (without detecting utf8)\n");
+    fprintf(stderr, "\t-P##\tuse non-default port ## for ftp control connection\n");
+    fprintf(stderr, "\t-t###\tconnection timeout ### in miliseconds (default is " _TOSTRING(DEF_TIMEOUTS) ")\n");
+	fprintf(stderr, "\t-h\tprint this help\n");
+    exit(err);
+}
+
+int main(int argc, char *argv[])
+{
+#if defined(_WIN32) && defined(_DEBUG)
+	_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_DEBUG );
+	_CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_DEBUG );
+	_CrtSetReportMode( _CRT_ASSERT, _CRTDBG_MODE_DEBUG );
+	_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG));
+#endif // _DEBUG
+
+    struct dt_dentry d = {DT_DIR, const_cast<char*>(""), 0, NULL, NULL, NULL, 0};
+    CFtpControlEx curdir;
+    bool full = false, lookup = false;
+    char *host;
+
+    CFtpControl::DefaultAnsiCP = DEFAULT_ANSI_CODEPAGE;
+
+	int i = 0;
+	while (++i < argc && argv[i][0] == '-') {
+		if (argv[i][1] == '-' && argv[i][2] == 0) {
+			i++;
+			break;
+		}
+		switch (argv[i][1]) {
+			case 'l':
+				lookup = true;
+				break;
+			case 'f':
+				full = true;
+				break;
+			case 'c':
+				CFtpControl::DefaultAnsiCP = argv[++i];
+				break;
+			case 'C':
+				curdir.SetConnCP(argv[++i]);
+				break;
+			case 'P':
+				curdir.ServerPORT = atoi(argv[i]+2);
+				break;
+			case 't':
+				curdir.timeouts = atoi(argv[i]+2);
+				break;
+			case 'u':
+				curdir.login = argv[++i];
+				break;
+			case 'p':
+				curdir.pass = argv[++i];//TODO: read from stdin
+				break;
+			case 'h':
+				usage(argv[0], EXIT_SUCCESS);
+			default:
+				usage(argv[0], EXIT_FAILURE);
+		}
+	}
+	if (i+1 != argc)
+		usage(argv[0], EXIT_FAILURE);
+	host = argv[i];
+
+	curdir.ServerIP = inet_addr(host);
+
+	try {
+		TryReconnect(&curdir);
+		if (lookup) {
+			if (struct dt_dentry *probe = walker.readdir(&curdir))
+				return dt_free(probe), EXIT_SUCCESS;
+			else
+				return EXIT_FAILURE;
+		}
+		if (full)
+			dt_full(&walker, &d, &curdir);
+		else
+			dt_reverse(&walker, &d, &curdir);
+	} catch(const CFtpControl::NetworkError&) {
+		return EXIT_FAILURE;
+	}
+
+    return EXIT_SUCCESS;
+}
+
