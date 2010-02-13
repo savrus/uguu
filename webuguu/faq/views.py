2ea653db8d84d7383c2583b4a909a737e390da27
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-02-13 14:52:55 +0300

    [search] echo SQL queries in results page

---
+++ webuguu/faq/views.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# webuguu.vfs.views - vfs view for django framework
+#
+# Copyright 2010, savrus
+# Read the COPYING file in the root of the source tree.
+#
+
+from django.shortcuts import render_to_response
+import string
+from webuguu.common import known_filetypes, known_protocols
+
+def bold(x):
+    return u"<b>%s</b>" % x
+
+def strargs(args):
+    return string.join([bold(x) for x in args], ", ")
+
+ru_sizedesc = u" Размером является десятичное число с последующим спецификатором. Спецификатором может быть одно из: tb, gb, mb, kb, byte."
+
+ru_multiarg = u" Moжно задать несколько аргументов через запятую, но не разделяя пробелом."
+
+ru_orders = {
+    'avl':      u"доступность шары",
+    'scan':     u"время последнего сканирования",
+    'uptime':   u"время последнего изменения доступности",
+    'proto':    u"протокол",
+    'net':      u"сеть",
+    'host':     u"имя хоста",
+    'size':     u"размер файла",
+    'name':     u"имя файла",
+    'type':     u"тип файла",
+    'sharesize':     u"размер шары целиком",
+}
+
+ru_matches = {
+    'name':     u"полнотекстовый поиск в имени файла",
+    'path':     u"полнотекстовый поиск в полной имени директории",
+    'full':     u"полнотекстовый поиск в полном пути (медленный вариант)",
+    'exact':    u"точное совпадение имени файла (будьте внимательны, между именем файла в запросе и опциями должен быть ровно один пробел)",
+}
+
+def ru_arg_desc(option):
+    return string.join([ u"%s: %s" % (bold(x), option[x]) for x in option.keys()], ", ")
+
+
+ru_qoptions = (
+    {'option': "match",
+     'args': strargs(ru_matches.keys()),
+     'desc': u"Критерий поиска. Аргументы означают следующее. " + ru_arg_desc(ru_matches) + u"."
+    },
+    {'option': "max",
+     'args': strargs(["100gb", "256.8mb"]),
+     'desc': u"Максимальный размер файла." + ru_sizedesc,
+    },
+    {'option': "min",
+     'args': strargs(["100gb", "256.8mb"]),
+     'desc': u"Минимальный размер файла." + ru_sizedesc
+    },
+    {'option': "type",
+     'args': strargs(tuple(known_filetypes) + ("dir",)),
+     'desc': u"Искать только среди файлов данного типа." + ru_multiarg
+    },
+    {'option': "proto",
+     'args': strargs(known_protocols),
+     'desc': u"Искать только в шарах с заданным протоколом." + ru_multiarg,
+    },
+    {'option': "host",
+     'args': "<i>hostname</i>",
+     'desc': u"Искать только в шарах с заданным именем хоста." + ru_multiarg,
+    },
+    {'option': "port",
+     'args': "<i>integer</i>",
+     'desc': u"Искать только в шарах на заданном порту. Для шар на дефолтном порту нужно указывать значение 0." + ru_multiarg,
+    },
+    {'option': "network",
+     'args': "<i>network</i>",
+     'desc': u"Искать только в заданной сети." + ru_multiarg,
+    },
+    {'option': "avl",
+     'args': strargs(["online", "offline"]),
+     'desc': u"Искать только в доступных/недоступных шарах",
+    },
+    {'option': "order",
+     'args': strargs(tuple(ru_orders.keys()) + \
+                     tuple(["%s.d" % x for x in ru_orders.keys()])),
+     'desc': u"Сортировать результаты в заданном порядке. Aргументы с суффиксом <b>.d</b> означают сортировку в обратном порядке." + ru_multiarg + u" Аргументы означают следующее. " + ru_arg_desc(ru_orders) + u".",
+    },
+)
+
+
+def ru(request):
+    return render_to_response("faq/ru.html",
+        {'options': ru_qoptions,
+         'gpage': "http://code.google.com/p/uguu",
+         'ggroup': "http://groups.google.com/group/uguu",
+        })
+
---
+++ webuguu/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^$',         include('webuguu.search.urls')),
+    (r'^search/',   include('webuguu.search.urls')),
+    (r'^light/$',   'webuguu.search.views.light'),
+    (r'^vfs/',      include('webuguu.vfs.urls')),
+    (r'^faq/',      include('webuguu.faq.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
