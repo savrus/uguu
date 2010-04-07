d3d0dd3272525a5ec30bb4e32f6f4e4a6a091aa3
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-04-07 10:30:11 +0400

    [libuguu] Cosmetics. Change tab to 4 spaces.

--- scanners/libuguu/getpass.c
+++ scanners/libuguu/getpass.c
@@ -23,61 +23,61 @@
 
 static void gp_set(gp_echo_mode echo, gp_save *gp)
 {
-	static HANDLE hIn = (HANDLE)-3;
-	if (hIn == INVALID_HANDLE_VALUE || hIn == 0) {
-		hIn = INVALID_HANDLE_VALUE;
-		return;
-	} else if ((HANDLE)-3 == hIn)
-		hIn = GetStdHandle(STD_INPUT_HANDLE);
-	switch (echo) {
-	case GP_ECHO_RESTORE:
-		SetConsoleMode(hIn, *gp);
-		break;
-	case GP_ECHO_ON:
-	case GP_ECHO_OFF:
-		if (GetConsoleMode(hIn, gp) == 0)
-			hIn = INVALID_HANDLE_VALUE;
-		else
-			SetConsoleMode(hIn, echo
-				? *gp | ENABLE_ECHO_INPUT
-				: *gp & ~ENABLE_ECHO_INPUT);
-	}
+    static HANDLE hIn = (HANDLE)-3;
+    if (hIn == INVALID_HANDLE_VALUE || hIn == 0) {
+        hIn = INVALID_HANDLE_VALUE;
+        return;
+    } else if ((HANDLE)-3 == hIn)
+        hIn = GetStdHandle(STD_INPUT_HANDLE);
+    switch (echo) {
+    case GP_ECHO_RESTORE:
+        SetConsoleMode(hIn, *gp);
+        break;
+    case GP_ECHO_ON:
+    case GP_ECHO_OFF:
+        if (GetConsoleMode(hIn, gp) == 0)
+            hIn = INVALID_HANDLE_VALUE;
+        else
+            SetConsoleMode(hIn, echo
+                ? *gp | ENABLE_ECHO_INPUT
+                : *gp & ~ENABLE_ECHO_INPUT);
+    }
 }
 
 #else /* _WIN32 */
 
 static void gp_set(gp_echo_mode echo, gp_save *gp)
 {
-	gp_save newgp;
-	switch (echo) {
-	case GP_ECHO_RESTORE:
-		tcsetattr(fileno(stdin), TCSADRAIN, gp);
-		break;
-	case GP_ECHO_ON:
-	case GP_ECHO_OFF:
-		tcgetattr(fileno(stdin), gp);
-		newgp = *gp;
-		if (echo == GP_ECHO_ON)
-			newgp.c_lflag |= ECHO;
-		else
-			newgp.c_lflag &= ~ECHO;
-		tcsetattr(fileno(stdin), TCSADRAIN, &newgp);
-	}
+    gp_save newgp;
+    switch (echo) {
+    case GP_ECHO_RESTORE:
+        tcsetattr(fileno(stdin), TCSADRAIN, gp);
+    break;
+    case GP_ECHO_ON:
+    case GP_ECHO_OFF:
+        tcgetattr(fileno(stdin), gp);
+        newgp = *gp;
+        if (echo == GP_ECHO_ON)
+            newgp.c_lflag |= ECHO;
+        else
+            newgp.c_lflag &= ~ECHO;
+        tcsetattr(fileno(stdin), TCSADRAIN, &newgp);
+    }
 }
 
 #endif /* _WIN32 */
 
 unsigned int gp_readline(char *buf, unsigned int size)
 {
-	gp_save gp;
-	gp_set(GP_ECHO_OFF, &gp);
-	buf[size - 1] = 0;
-	buf = fgets(buf, size, stdin);
-	gp_set(GP_ECHO_RESTORE, &gp);
-	if (buf && (size = strlen(buf)) > 0 && buf[size - 1] == '\n') {
-		buf[size - 1] = 0;
-		return size - 1;
-	} else
+    gp_save gp;
+    gp_set(GP_ECHO_OFF, &gp);
+    buf[size - 1] = 0;
+    buf = fgets(buf, size, stdin);
+    gp_set(GP_ECHO_RESTORE, &gp);
+    if (buf && (size = strlen(buf)) > 0 && buf[size - 1] == '\n') {
+        buf[size - 1] = 0;
+        return size - 1;
+    } else
         return 0;
 }
 
