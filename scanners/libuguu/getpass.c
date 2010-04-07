950ce9d87714595f550cfe42ce506f6b9d1d4b98
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-04-07 10:11:23 +0400

    [libuguu] Fix some misprints and cosmetics in getpass.

--- scanners/libuguu/getpass.c
+++ scanners/libuguu/getpass.c
@@ -1,6 +1,7 @@
 /* getpass - echoless input from stdin
 *
-* Copyright 2009, 2010, savrus
+* Copyright 2010, Radist <radist.nt@gmail.com>
+* Copyright 2010, savrus
 * Read the COPYING file in the root of the source tree.
 */
 
@@ -17,8 +18,10 @@
 #include "getpass.h"
 
 #ifdef _WIN32
+
 #define INHNO_ERROR (-2)
-void gp_set(echo_mode echo, gp_save *gp)
+
+static void gp_set(gp_echo_mode echo, gp_save *gp)
 {
 	static HANDLE hIn = (HANDLE)-3;
 	if (hIn == INVALID_HANDLE_VALUE || hIn == 0) {
@@ -27,11 +30,11 @@ void gp_set(echo_mode echo, gp_save *gp)
 	} else if ((HANDLE)-3 == hIn)
 		hIn = GetStdHandle(STD_INPUT_HANDLE);
 	switch (echo) {
-	case ECHO_RESTORE:
+	case GP_ECHO_RESTORE:
 		SetConsoleMode(hIn, *gp);
 		break;
-	case ECHO_ON:
-	case ECHO_OFF:
+	case GP_ECHO_ON:
+	case GP_ECHO_OFF:
 		if (GetConsoleMode(hIn, gp) == 0)
 			hIn = INVALID_HANDLE_VALUE;
 		else
@@ -40,37 +43,41 @@ void gp_set(echo_mode echo, gp_save *gp)
 				: *gp & ~ENABLE_ECHO_INPUT);
 	}
 }
-#else
-void gp_set(int echo_on, gp_save *gp)
+
+#else /* _WIN32 */
+
+static void gp_set(gp_echo_mode echo, gp_save *gp)
 {
 	gp_save newgp;
 	switch (echo) {
-	case ECHO_RESTORE:
+	case GP_ECHO_RESTORE:
 		tcsetattr(fileno(stdin), TCSADRAIN, gp);
 		break;
-	case ECHO_ON:
-	case ECHO_OFF:
+	case GP_ECHO_ON:
+	case GP_ECHO_OFF:
 		tcgetattr(fileno(stdin), gp);
 		newgp = *gp;
-		if (echo)
-			gp->c_lflag |= ECHO;
+		if (echo == GP_ECHO_ON)
+			newgp.c_lflag |= ECHO;
 		else
-			gp->c_lflag &= ~ECHO;
-		tcsetattr(fileno(stdin), TCSADRAIN, gp);
+			newgp.c_lflag &= ~ECHO;
+		tcsetattr(fileno(stdin), TCSADRAIN, &newgp);
 	}
 }
-#endif
+
+#endif /* _WIN32 */
 
 unsigned int gp_readline(char *buf, unsigned int size)
 {
 	gp_save gp;
-	gp_set(ECHO_OFF, &gp);
-	buf[size-1] = 0;
+	gp_set(GP_ECHO_OFF, &gp);
+	buf[size - 1] = 0;
 	buf = fgets(buf, size, stdin);
-	gp_set(ECHO_RESTORE, &gp);
-	if (buf && (size = strlen(buf)) > 0 && '\n' == buf[size-1]) {
-		buf[size-1] = 0;
-		return size-1;
-	} else return 0;
+	gp_set(GP_ECHO_RESTORE, &gp);
+	if (buf && (size = strlen(buf)) > 0 && buf[size - 1] == '\n') {
+		buf[size - 1] = 0;
+		return size - 1;
+	} else
+        return 0;
 }
 
