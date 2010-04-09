7db2fcef7e7cfc748f532ce0611896b2e1492230
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-04-09 09:42:17 +0400

    [vfs] Add user-selective ordering by size, name

--- webuguu/vfs/views.py
+++ webuguu/vfs/views.py
@@ -57,6 +57,13 @@ def sharelist(request, column, name, is_this_host):
         return render_to_response('vfs/error.html',
             {'error':"Unable to connect to the database."})
     cursor = db.cursor()
+    try:
+        order = request.GET.get('order')
+        url = dict()
+        url['order'] = [('order', order)] if order != None else []
+    except:
+        return render_to_response('vfs/error.html',
+            {'error':"Wrong parameters."})
     cursor.execute("""
         SELECT 
             count(*) AS items,
@@ -75,19 +82,29 @@ def sharelist(request, column, name, is_this_host):
         return render_to_response('vfs/error.html',
             {'error':"No shares within %s '%s'" % (column, name)})
     offset, gobar = offset_prepare(request, items, vfs_items_per_page)
+    orders = {
+        'size': "size DESC",
+        'name': "state, hostname"
+    }
+    if orders.get(order):
+        order = orders[order]
+    else:
+        order = orders['name']
     cursor.execute("""
         SELECT id as share_id, state, size, network, protocol, hostname, port
         FROM shares
         WHERE %s = %%(n)s
-        ORDER BY state, hostname
+        ORDER BY %s
         OFFSET %%(o)s LIMIT %%(l)s
-        """ % column, {'n':name, 'o':offset, 'l':vfs_items_per_page})
-    fastselflink = "./?"
+        """ % (column, order), {'n':name, 'o':offset, 'l':vfs_items_per_page})
+    fastselforderedlink = "./?"
+    fastselflink = "./?" + urlencode(url['order'])
     return render_to_response("vfs/sharelist.html", \
         {'name': name,
          'ishost': is_this_host,
          'shares': cursor.fetchall(),
          'fastself': fastselflink,
+         'fastorder': fastselforderedlink,
          'gobar': gobar,
          'info': listinfo,
          'gentime': time.time() - generation_started,
@@ -117,10 +134,12 @@ def share(request, proto, hostname, port, path=""):
         id = int(request.GET.get('s', 0))
         path_id = int(request.GET.get('p', 0))
         page_offset = int(request.GET.get('o', 0))
+        order = request.GET.get('order')
         url = dict()
         url['share'] = [('s', id)]
         url['path'] = [('p', path_id)]
         url['offset'] = [('o', page_offset)] if page_offset > 0 else []
+        url['order'] = [('order', order)] if order != None else []
     except:
         return render_to_response('vfs/error.html',
             {'error':"Wrong parameters."})
@@ -191,15 +210,32 @@ def share(request, proto, hostname, port, path=""):
     # detect offset in file list and fill offset bar
     offset, gobar = offset_prepare(request, items, vfs_items_per_page)
     # get file list
-    cursor.execute("""
-        SELECT sharedir_id AS dirid, size, name
-        FROM files
-        WHERE share_id = %(s)s
-            AND sharepath_id = %(p)s
-            AND pathfile_id >= %(o)s
-        ORDER BY pathfile_id
-        LIMIT %(l)s;
-        """, {'s': share_id, 'p': path_id, 'o': offset, 'l':vfs_items_per_page})
+    orders = {
+        'size': """
+            SELECT sharedir_id AS dirid, size, name
+            FROM files
+            WHERE share_id = %(s)s
+                AND sharepath_id = %(p)s
+            ORDER BY size DESC
+            OFFSET %(o)s
+            LIMIT %(l)s;
+            """,
+        'name': """
+            SELECT sharedir_id AS dirid, size, name
+            FROM files
+            WHERE share_id = %(s)s
+                AND sharepath_id = %(p)s
+                AND pathfile_id >= %(o)s
+            ORDER BY pathfile_id
+            LIMIT %(l)s;
+            """
+    }
+    if orders.get(order):
+        query = orders[order]
+    else:
+        query = orders['name']
+    cursor.execute(query, {'s': share_id, 'p': path_id, 'o': offset, 
+                           'l':vfs_items_per_page})
     # some additional variables for template
     if hostaddr == None:
         hostaddr = hostname
@@ -216,7 +252,8 @@ def share(request, proto, hostname, port, path=""):
             ([('o', uplink_offset)] if uplink_offset > 0 else []) ))
     else:
         fastuplink = ""
-    fastselflink = "./?" + urlencode(dict(url['share'] + url['path']))
+    fastselforderedlink = "./?" + urlencode(dict(url['share'] + url['path']))
+    fastselflink = "./?" + urlencode(dict(url['share'] + url['path'] + url['order']))
     return render_to_response('vfs/share.html', \
         {'files': cursor.fetchall(),
          'protocol': proto,
@@ -229,6 +266,7 @@ def share(request, proto, hostname, port, path=""):
          'share_id': id,
          'fastup': fastuplink,
          'fastself': fastselflink,
+         'fastorder': fastselforderedlink,
          'offset': offset,
          'gobar': gobar,
          'state': state,
