2a76180d8b91a61d60533e9dccc014156e3b4e80
savrus <savrus@localhost>
2010-04-19 01:16:12 +0400

    [libuguu.dtread] Module to read 'reversed' output of dt back into tree

---
+++ scanners/libuguu/dtread.h
@@ -0,0 +1,30 @@
+/* dtread - definitions and interfaces of tree reading
+ *
+ * Copyright 2010, savrus
+ * Read the COPYING file in the root of the source tree.
+ */
+
+#ifndef DTREAD_H
+#define DTREAD_H
+
+#include "dt.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct dtread_data {
+    struct dt_dentry *de;
+    struct dt_dentry *child;
+    struct dt_dentry *file_child;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+/* reconstruct tree from file. Returns NULL on failure */
+struct dt_dentry * dtread_readfile(const char *filename);
+
+#endif /* DTREAD_H */
+
