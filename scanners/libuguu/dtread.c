91852f9b625c80c1033408f89bb6a291dfebc55b
Ruslan Savchenko <ruslan.savchenko@gmail.com>
2010-05-11 11:19:50 +0400

    [libuguu.dtread] cosmetics

--- scanners/libuguu/dtread.c
+++ scanners/libuguu/dtread.c
@@ -111,8 +111,8 @@ static int dtread_readline(const char *line, struct cuckoo_ctx *cu, unsigned int
 
             if (id == 0) {
                 /* file */
-                LOG_ASSERT( pid != 0, "File with parent id=0 (only root dir has parent id=0). Line %s\n",
-                    line);
+                LOG_ASSERT(pid != 0, "File with parent id=0 "
+                    "(only root dir has parent id=0). Line %s\n", line);
 
                 if ((d = dt_alloc()) == NULL) {
                     free(name);
