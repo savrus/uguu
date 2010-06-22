5ff524df83df5acbcbc08c60f4079de36437f8e8
Radist <radist.nt@gmail.com>
2010-06-22 04:29:46 +0400

    [dump.py] dump only shares with non-empty diffs

--- misc/save/dump.py
+++ misc/save/dump.py
@@ -13,18 +13,41 @@ import subprocess
 
 from common import connectdb, scanners_path, db_host, db_user, db_password, db_database, share_save_str, share_save_path
 
+shares_dump_dir = 'dump'
+shares_dump_dir = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), shares_dump_dir)
+
+def create_dump_dir():
+     if os.path.isdir(shares_dump_dir):
+        return
+     if os.path.isfile(shares_dump_dir):
+        print "%s should be a directory, not a file\n" % (shares_dump_dir,)
+        sys.exit()
+     print "%s directory doesn't exist, creating" % (shares_dump_dir,)
+     os.mkdir(shares_dump_dir)
+
+def zero_diff(filename):
+   firstline = True
+   for line in open(filename, "rt"):
+       if firstline:
+           firstline = False
+           if line[0] != '*':
+               return True
+       else:
+           return line[0] not in ('+','-','*')
+
 def dump_share(share, diffto = ""):
     print "Dumping %(proto)s://%(host)s:%(port)s" \
         % {'proto': share['protocol'],
            'port': str(share['port']),
            'host': share['hostname']}
     cmd = os.path.join(scanners_path, "sqlscan")
+    outfile = os.path.join(shares_dump_dir, share_save_str(share['protocol'], share['hostname'], share['port']))
     sp = subprocess.Popen("%(cmd)s -s %(proto)s -p %(port)s -dP %(dh)s %(du)s %(dd)s %(dfile)s %(host)s > %(sfile)s"
         % {'cmd': cmd,
            'proto': share['protocol'],
            'port': str(share['port']),
            'host': share['hostname'],
-           'sfile': os.path.join('dump', share_save_str(share['protocol'], share['hostname'], share['port'])),
+           'sfile': outfile,
            'dfile': "-u " + diffto if diffto != "" else "",
            'dh': "-dh " + db_host if len(db_host) > 0 else "",
            'du': "-du " + db_user if len(db_user) > 0 else "",
@@ -33,8 +56,13 @@ def dump_share(share, diffto = ""):
         shell = True, stdin=subprocess.PIPE)
     sp.communicate(input = db_password + "\n")
     sp.wait()
+    return outfile
 
 if __name__ == "__main__":
+    if len(set(('?','-?','/?','-h','/h','help','--help')).intersection(sys.argv)) > 0:
+        print sys.argv[0], '[diff [only]]'
+        print "diff\tdump with diffs"
+        print "only\tleave only dumps with non-empty diffs"
     try:
         db = connectdb()
     except:
@@ -44,11 +72,18 @@ if __name__ == "__main__":
     shares.execute("""
         SELECT share_id, protocol, hostname, port FROM shares
         """)
-    for share in shares.fetchall():
-        if "diff" in sys.argv:
+    create_dump_dir()
+    skipmode = 'only' in sys.argv
+    if 'diff' in sys.argv:
+        for share in shares.fetchall():
             savepath = share_save_path(share['protocol'], share['hostname'], share['port'])
             if os.path.isfile(savepath):
-                dump_share(share,savepath)
-                continue
-        dump_share(share)
+                dump = dump_share(share,savepath)
+                if skipmode and zero_diff(dump):
+                    os.unlink(dump)
+            elif not skipmode:
+                dump_share(share)
+    else:
+        for share in shares.fetchall():
+            dump_share(share)
 
